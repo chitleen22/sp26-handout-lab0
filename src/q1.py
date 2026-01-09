da6d71c908b7c290a923b855d6a36dc94ec16b23
Chitleen Kaur <Kaur.chi@northeastern.edu>
2026-01-09 12:05:09 -0800

    save changes moved around code for cleaning purposes

--- src/q1.py
+++ src/q1.py
@@ -2,10 +2,7 @@
 Please implement the stub function to match the documentation.
 Make sure to implement tests in the tests directory.
 """
-
-
-def is_palindrome(s: str) -> bool:
-    """
+"""
     Check if the given string is a palindrome.
 
     A palindrome is a string that reads the same forwards and backwards,
@@ -16,6 +13,9 @@ def is_palindrome(s: str) -> bool:
     Returns:
         bool: True if the string is a palindrome, False otherwise.
     """
+
+def is_palindrome(s: str) -> bool:
+    
     cleaned = ""
     for char in s:
         if char.isalnum():
