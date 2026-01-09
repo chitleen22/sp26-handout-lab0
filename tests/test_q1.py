8028e75db1847c8fe0388e2ff2febd11d4969f79
Chitleen Kaur <Kaur.chi@northeastern.edu>
2026-01-08 20:33:08 -0800

    wrote down tests for the function

--- tests/test_q1.py
+++ tests/test_q1.py
@@ -1,6 +1,25 @@
 """Tests for Lab 0 Question 1"""
 
+from src.q1 import is_palindrome
+import unittest
 import sys
 sys.path.append(".")
-import unittest
-from src.q1 import is_palindrome
+
+
+class TestIsPalindrome(unittest.TestCase):
+
+    def test_with_palindrome(self):
+        self.assertTrue(is_palindrome("racecar"))
+
+    def test_case_insensetive(self):
+        self.assertTrue( is .is_palindrome("RaCeCaR"))
+
+    def test_with_spaces(self):
+        self.assertTrue(is_palindrome("taco cat"))
+
+    def test_yellow(self):
+        self.assertFalse(is_palindrome("yellow"))
+
+
+if __name__ == "__main__":
+    unittest.main()
